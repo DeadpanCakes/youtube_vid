130daa8fd5b6406658135f11cf1a11972b9512b5
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-07 17:48:58 -0700

    changed Dropdown to allow change in how text is justified

--- src/Components/Dropdown/Dropdown.js
+++ src/Components/Dropdown/Dropdown.js
@@ -1,10 +1,14 @@
 import { cloneElement } from "react";
 import style from "./Dropdown.module.css";
 
-const Dropdown = ({ children }) => {
+const Dropdown = ({ children, alignment }) => {
   const styledChildren = children.map((child, key) => {
     console.log(key);
-    return cloneElement(child, { key, className: style.dropdownItem });
+    return cloneElement(child, {
+      key,
+      className: style.dropdownItem,
+      style: { justifyContent: alignment },
+    });
   });
   return <ul className={style.dropdown}>{styledChildren}</ul>;
 };
