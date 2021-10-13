4fade6b5813cd239c59d68e990e2a3277c2584c5
Anthony Mendoza <anthony.mendoza310@gmail.com>
2021-10-12 19:02:03 -0700

    implemented recommended components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import PageHeader from "./Components/PageHeader/PageHeader";
+import MainVideo from "./Components/MainVideo/MainVideo";
+import Recommendation from "./Components/Recommendation/Recommendation";
 
 function App() {
   return (
     <div className="App">
       <PageHeader></PageHeader>
+      <div className="content">
+        <MainVideo />
+        <Recommendation />
+      </div>
     </div>
   );
 }
