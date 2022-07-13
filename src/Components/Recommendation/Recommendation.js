d8dfd506f5e6131f708c27be641720f746c5ee95
DeadpanCakes <anthony.mendoza310@gmail.com>
2022-07-13 15:54:47 -0700

    added style to container

--- src/Components/Recommendation/Recommendation.js
+++ src/Components/Recommendation/Recommendation.js
@@ -1,9 +1,10 @@
 import db from "./recommendationsDB";
 import RecommendedVid from "./RecommendedVid";
+import style from "./Recommendation.module.css";
 
 const Recommendation = () => {
   return (
-    <aside>
+    <aside className={style.recommendation}>
       <ul>
         {db.map((vid) => {
           const { imgSrc, imgDesc, title, channel, views, date } = vid;
